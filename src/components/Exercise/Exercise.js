a554acd367917d2bbdc626f1fec6f81f7c6f8d9e
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-09-28 19:01:39 +0300

    Ui Design

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Exercise = () => {
     return (
-        <div className='exercise-area'>
-                    
+        <div className="exercise-area">
+            <h2>Home-Fitness-Club</h2>
+            <p>Select today’s exercise</p>
+
+            <div className="exercises">
+                <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Dumble</Card.Title>
+                        <Card.Text>
+                            Amet minim mollit non deserunt ullamco est sit
+                            aliqua dolor do amet sint
+                        </Card.Text>
+                        <Card.Text>
+                            <span>For Age</span> : 20-25
+                        </Card.Text>
+                        <Card.Text>
+                            <span>Time required</span> : 30s
+                        </Card.Text>
+                        <Button variant="primary">Add To List</Button>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     );
 };
